b45432c85a8e29954d780053ac94d5356bd6bd29
SHIVAM CHAUDHARY <Shivamchy076@gmail.com>
2025-06-30 16:09:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const ham =document.getElementById("ham")
+const cros =document.getElementById("cros")
+const navbar=document.getElementById("menue")
+ ham.addEventListener("click",()=>{
+   menue.style.display="flex"
+ });
+ cros.addEventListener("click",()=>{
+    menue.style.display="none"
+ })
