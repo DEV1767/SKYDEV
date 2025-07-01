5838e307d688da0f930f88ab364b8498e69d81b9
SHIVAM CHAUDHARY <Shivamchy076@gmail.com>
2025-07-01 08:35:28 +0530

    Delete 1script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const ham =document.getElementById("ham")
-const cros =document.getElementById("cros")
-const navbar=document.getElementById("menue")
- ham.addEventListener("click",()=>{
-   menue.style.display="flex"
- });
- cros.addEventListener("click",()=>{
-    menue.style.display="none"
- })
